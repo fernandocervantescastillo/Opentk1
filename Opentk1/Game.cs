49de7a2fb8625885d5579ee79bfd5d9489dc40ca
fernandocervantescastillo <50065506+fernandocervantescastillo@users.noreply.github.com>
2022-05-07 11:11:36 -0400

    Agregar archivos de proyecto.

---
+++ Opentk1/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+using System.Drawing;
+namespace Opentk1
+{
+    class Game : GameWindow
+    {
+
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color4.Black);
+
+
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            //GL.DeleteBuffer(VertexBufferObject);
+            base.OnUnload(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.DepthMask(true);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+            GL.LoadIdentity();
+
+            GL.Begin(PrimitiveType.Polygon);
+            GL.Color3(Color.Aqua);
+            GL.Vertex3(0,0,0);
+            GL.Vertex3(10, 0, 0);
+            GL.Vertex3(10, 10, 0);
+            GL.Vertex3(5, 15, 0);
+            GL.Vertex3(0, 10, 0);
+            GL.End();
+
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            float d = 30;
+            GL.Viewport(0, 0, Width, Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-d, d, -d, d, -d, d);
+            //GL.Frustum(-80, 80, -80, 80, 4, 100);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            base.OnResize(e);
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+        }
+
+
+
+    }
+}
---
+++ Opentk1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Opentk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game juego = new Game(800, 600, "Casa");
+            juego.Run(60);
+        }
+    }
+}
